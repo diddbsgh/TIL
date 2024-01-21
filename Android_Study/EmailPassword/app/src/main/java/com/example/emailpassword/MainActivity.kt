388da794e1abba50f5ae9c78355fb557bed11e9c
yang-yunho <yangyoonho0710@naver.com>
2024-01-22 01:03:42 +0900

    ADD::TextField(email)

--- Android_Study/EmailPassword/app/src/main/java/com/example/emailpassword/MainActivity.kt
+++ Android_Study/EmailPassword/app/src/main/java/com/example/emailpassword/MainActivity.kt
@@ -3,14 +3,34 @@ package com.example.emailpassword
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.emailpassword.ui.theme.EmailPasswordTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +40,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White
                 ) {
-                    Greeting("Android")
+                    EmailPassword()
                 }
             }
         }
@@ -30,12 +50,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun EmailPassword(){
+    // 이메일, 비밀번호 값 저장하는 상태
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.email_label),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding())
+        TextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            keyboardOptions = KeyboardOptions(
+                keyboardType= KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            singleLine = true,
+            trailingIcon = {
+                Text(text = "@dsm.hs.kr",
+                    modifier = Modifier.padding(end = 16.dp))
+            },
+            placeholder = {
+                Text(text = "example")
+            },)
+            }
+    }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     EmailPasswordTheme {
-        Greeting("Android")
+        EmailPassword()
     }
 }
